df926d8a1b3fdbb3d14035a15bc981684de7dbc1
cong thang <ntcongthang@gmail.com>
2015-09-29 15:44:51 +0700

    change connect

---
+++ model/connect.php
@@ -0,0 +1,28 @@
+<?php
+class connect {
+    var $db = null;
+    function __construct() {
+        $dsn = "mysql:host=localhost;dbname=";
+        $user = 'root';
+        $pass ='';
+        $this -> db = new PDO($dsn, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    }
+
+    function getList($select){
+        $result = $this -> db -> query($select);
+        return $result;
+    }
+
+    function getInstance($select){
+        $results = $this -> db -> query($select);
+        $result = $results -> fetch();
+        return $result;
+    }
+
+    function exec($query){
+        $result = $this -> db -> exec($query);
+        return $result;
+    }
+
+} 
+?>
