23f6a7ea47f752a89be61ec1df2299fb5afaadc4
Nguyễn Trần Công Thắng <ntcongthang@gmail.com>
2015-10-03 16:35:27 +0700

    change user controller

---
+++ controller/usercontroller.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+if(isset($_POST["action"]))
+	$action = $_POST["action"];
+elseif (isset($_GET["action"]))
+ 	$action = $_GET["action"];
+ else
+ 	$action = "newList";
+
+ $_SESSION[""] = "";
+
+ switch ($action) {
+ 	case 'home':
+ 		include "../view/";
+ 		break;
+ 	case 'newList':
+ 		include "../view/";
+ 		break;
+ 	case 'contact':
+ 		include "../view/";
+ 		break;
+
+ }
+?>
