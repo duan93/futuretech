b8acc0fb05e4d0845db09807ddc594c7e8fa1085
Nguyễn Trần Công Thắng <ntcongthang@gmail.com>
2015-10-04 17:55:27 +0700

    change database

--- model/connect.php
+++ model/connect.php
@@ -2,7 +2,7 @@
 class connect {
     var $db = null;
     function __construct() {
-        $dsn = "mysql:host=localhost;dbname=";
+        $dsn = "mysql:host=localhost;dbname=futuretech";
         $user = 'root';
         $pass ='';
         $this -> db = new PDO($dsn, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
